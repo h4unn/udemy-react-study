5a63ef94f2537b639f993c04e71590fe1ac8170a
현남 <wiwi154@naver.com>
2023-08-16 17:49:01 +0900

    '0816update'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -12,10 +12,20 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <p>This is also visible!</p>
+      {/* 
       <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/> 
+        아래처럼 단일 props 를  expenseItem 전달하는 방식은 
+        전달은 편리하지만 하위 componet에선 분리를 시켜 사용해야 한다.
+        ex) props.expense.title >> 전달 key(?)가 expense
+      */}
+      {/* <ExpenseItem expense={expenses[0]}/>
+      <ExpenseItem expense={expenses[1]}/>
+      <ExpenseItem expense={expenses[2]}/>
+      <ExpenseItem expense={expenses[3]}/> */}
+      <Expenses items={expenses}/>
     </div>
   );
 }
---
+++ src/components/Expenses.js
@@ -0,0 +1,15 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import "./Expenses.css"
+function Expenses(props) {
+    console.log(props);
+    return (
+        <Card className="expenses">
+            <ExpenseItem expense={props.items[0]}/>
+            <ExpenseItem expense={props.items[1]}/>
+            <ExpenseItem expense={props.items[2]}/>
+            <ExpenseItem expense={props.items[3]}/>
+        </Card>
+    );
+}
+export default Expenses;  
